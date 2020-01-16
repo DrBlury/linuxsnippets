0c48033c04360ed385d72280b09085522d6c7419
Julian Bensch <Bensch.julian@gmail.com>
2020-01-16 15:28:33 +0100

    added a new special effect

--- fuzzy_lock.sh
+++ fuzzy_lock.sh
@@ -1,13 +1,16 @@
 #!/bin/sh -e
 
-# Take a screenshot
-scrot /tmp/screen_locked.png
+scrot /tmp/i3lock.png 
 
 # Pixellate it 10x
-mogrify -scale 20% -scale 500% /tmp/screen_locked.png
+#mogrify -scale 25% -scale 400% /tmp/i3lock.png
+
+mogrify -radial-blur 2 /tmp/i3lock.png
+mogrify -spread 4 /tmp/i3lock.png
+convert /tmp/i3lock.png ~/Pictures/lock.png -gravity southeast -geometry +10+10 -composite /tmp/i3lock.png
 
 # Lock screen displaying this image.
-i3lock -i /tmp/screen_locked.png
+i3lock -i /tmp/i3lock.png
 
 # Turn the screen off after a delay.
-sleep 60; pgrep i3lock && xset dpms force off
+# sleep 60; pgrep i3lock && xset dpms force off
